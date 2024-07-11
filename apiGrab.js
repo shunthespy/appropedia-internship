2401df7f289fa5ef65a0128491925d1f2a05a7dc
shunthespy <shunthespy@email.com>
2024-07-10 23:10:41 -0700

    fixed continue

--- apiGrab.js
+++ apiGrab.js
@@ -1,7 +1,21 @@
-const wikiEndPoint = "http://appropedia.org/w/api.php" //make this just appropedia and include end in fetch DONE (put in wikiSite)
 const botIDs = [116268, 119543, 120737, 120901]; 
 const wikiSite = "http://appropedia.org/"; //this is a temporary default and will be removed to make code more open to other sites
 let excludeBots = true; //gonna make this toggleable
+let currentCat = "";
+var continuing = false;
+let continueString = "";
+let timeFrame = 0; // ms back determined by buttons
+
+let apiLink = "";
+let proxy = "https://cors-anywhere-2mlo.onrender.com/";
+
+let totalContribs = 0;
+let topContribPage = "";
+let topContribCount = 0;
+let totalUnique = 0; //not really needed
+let allUnique = [];
+let topUserPage = "";
+let topUserPageCount = 0;
 
 var hashParams = window.location.hash.substring(1).split('&'); // substr(1) to remove the `#`
 for(var i = 0; i < hashParams.length; i++){
@@ -10,6 +24,7 @@ for(var i = 0; i < hashParams.length; i++){
 }
 
 function fetchWikiExtract(catGrab){ //builds api link with category as input
+    console.log("FETCHED");
     let wikiParams = 'w/api.php?action=query'
     + '&generator=categorymembers'
     + '&gcmlimit=100'
@@ -18,15 +33,9 @@ function fetchWikiExtract(catGrab){ //builds api link with category as input
     + '&prop=id'
     + "&format=json"
     + '&origin=*'
+    if(continuing) wikiParams += continueString += gsmContinueString;
     return wikiSite + wikiParams;
 }
-
-let apiLink = "";
-let proxy = "https://cors-anywhere-2mlo.onrender.com/";
-
-let totalContribs = 0;
-let topContribPage = "";
-let topContribCount = 0;
 
 //todo
 //add some metric for recency on contrib
@@ -35,58 +44,57 @@ let topContribCount = 0;
 //as a bonus feature, display top contributed page
 //also display total users contributed (unique users) and maybe top unique user page
 
-//500 page fix
+//100 page fix DONE
 //remove bot edits (currently cant)
-//url parameter TESTING
+//url parameter DONE
 //turn into object with functions to wrap (probably will do this at end)
 //list for users and pages (maybe like top users 10, show all button if over 10 total?)
 //push down old data, compare?
 //start date end date and predetermined buttons 1month 3month etc
-//check for unique users in a timeframe
+//check for unique users in a timeframe (most likely not doable?)
 //fix subpages in edit count DONE
 //fix href DONE
-async function setDisplay(nothing){
+async function setDisplay(){
     //console.log(apiLink);
-    apiLink = proxy + apiLink; //only needed for testing
-    let totalUnique = 0; //not really needed
-    let allUnique = [];
-    let topUserPage = "";
-    let topUserPageCount = 0;
-    fetch(apiLink).then(response => response.json()).then(//get data for category
+    fetch(proxy + apiLink).then(response => response.json()).then(//get data for category
         data => {
             console.log(data);
+            if (data.continue != null) { //this sets up continue at end of promise chain
+                continueString = "&continue=" + data.continue.continue;
+                gsmContinueString = "&gcmcontinue=" + data.continue.gcmcontinue;
+                continuing = true;
+                apiLink = fetchWikiExtract(currentCat);
+            } else {
+                continuing = false
+            }
             let bar = Object.keys(data.query.pages);//100x coder
             for(i = 0; i < bar.length; i++){
                 //get data for contributers
                 fetch(genIDBasedAPILinkContributors(bar[i])).then(response => response.json()).then(//get data for page
                     data => {
                         // console.log(data);
-                        let foo = data.query.pages;
-                        for (let page in foo) {
+                        let allPages = data.query.pages;
+                        for (let page in allPages) {
                             let tempUnique = 0
-                            let contributors = foo[page].contributors;
-                            console.log(contributors);
-                            // console.log(contributors[0]);
-                            // console.log(contributors[0].userid);
+                            let currentTitle = allPages[page].title
+                            let contributors = allPages[page].contributors;
                             if(contributors[0].userid==120901) break; //translation (only) check
                             for(j = 0; j < contributors.length; j++){
                                 let currentID = contributors[j].userid;
                                 if(botIDs.indexOf(currentID) != -1) continue; //check not bot
                                 tempUnique++; //add to unique user temp count if not bot
-                                if(allUnique.indexOf(currentID) == -1){
+                                if(allUnique.indexOf(currentID) == -1){ //if current user not yet recorded
                                     allUnique.push(currentID);
                                     totalUnique++;
                                 }
                             }
                             if (tempUnique > topUserPageCount) { //if this page is highest users
                                 topUserPageCount = tempUnique; //save num
-                                topUserPage = foo[page].title; //save title
+                                topUserPage = currentTitle; //save title
                             }
                             //needs to omit edits from bots
-                            setEdits(foo[page].title);
-                        };                        
-                        // console.log(totalUnique);
-                        // console.log(allUnique);
+                            setEdits(currentTitle);
+                        }; 
                         document.getElementById("totalUsers").innerText = "All unique users: " + totalUnique;
                         document.getElementById("highestUsers").innerText = "Top users in a page : " + topUserPageCount;
                         document.getElementById("highestUsersPage").innerText = "Top user page: " + topUserPage;
@@ -95,11 +103,15 @@ async function setDisplay(nothing){
                     }).catch(error => {console.error(error);})
                 //get data for contribs
             }   
-        }).catch(error => {console.error(error);})
+        }).then(function(){ //recur to continue
+            if(continuing)setDisplay();
+        })
+        .catch(error => {console.error(error);})
 }
 
-async function getEdits(page){
+async function getEdits(page){ //gets data for page edits
     let link = proxy + wikiSite + "w/rest.php/v1/page/" + encodeURIComponent(page) +"/history/counts/edits"
+    if (timeFrame!=0) link += ("?from=" + timeFrame + "&to=" + Date.now())
     try {
         var response = await fetch(link, {
             method: "GET"
@@ -112,7 +124,7 @@ async function getEdits(page){
     }
 }
 
-async function setEdits(page){//
+async function setEdits(page){ //gets page edits and then adds them to the counter
     let json = await getEdits(page);
     // console.log(json);
     // console.log(json.count)
@@ -135,21 +147,20 @@ function genIDBasedAPILinkContributors(id){ //finds all contributors in a page
     return proxy + wikiSite + "w/api.php?action=query&prop=contributors&pageids=" + id + "&format=json"
 }
 
-function genTitleBasedAPILink(title){ //currently unused
-    return "action=query&prop=contributors&titles=" + title + "&format=json"
-}
-
-function setLinkToInput(){ //sets apiLink to inputted category, then dumps info on page
+async function setLinkToInput(){ //sets apiLink to inputted category, then dumps info on page
     totalContribs = 0;
     topContribPage = "";
     topContribCount = 0;
-    let newCat = document.getElementById("category").value;
-    apiLink = fetchWikiExtract(newCat);
-    setDisplay(apiLink);
+    currentCat = document.getElementById("category").value;
+    apiLink = fetchWikiExtract(currentCat);
+    continuing = await setDisplay(apiLink);
+    console.log(continuing);
+    while(continuing) await setDisplay(apiLink)
 }
 
 document.getElementById("test").onclick = function(){ //temporary test function
-    apiLink = fetchWikiExtract("Tolocar");
+    currentCat = "Tolocar";
+    apiLink = fetchWikiExtract(currentCat);
     setDisplay(apiLink);
 }
 
